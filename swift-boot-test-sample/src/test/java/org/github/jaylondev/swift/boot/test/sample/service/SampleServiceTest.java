04f9e5e84a06d119ab18e97cc37787a2141fb93a
jay1on <nobug@88.com>
2023-10-31 22:39:55 +0800

    完善示例

--- swift-boot-test-sample/src/test/java/org/github/jaylondev/swift/boot/test/sample/service/SampleServiceTest.java
+++ swift-boot-test-sample/src/test/java/org/github/jaylondev/swift/boot/test/sample/service/SampleServiceTest.java
@@ -3,9 +3,14 @@ package org.github.jaylondev.swift.boot.test.sample.service;
 import lombok.extern.slf4j.Slf4j;
 import org.github.jaylondev.swift.boot.test.annotations.SwiftBootTest;
 import org.github.jaylondev.swift.boot.test.sample.base.BaseTest;
+import org.github.jaylondev.swift.boot.test.sample.dao.SampleRepositoryA;
+import org.github.jaylondev.swift.boot.test.sample.dao.mapper.SampleMapperA;
 import org.github.jaylondev.swift.boot.test.sample.dto.resp.SampleResp;
 import org.github.jaylondev.swift.boot.test.sample.utils.JsonUtils;
+import org.github.jaylondev.swift.boot.test.utils.MockUtils;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -17,9 +22,17 @@ public class SampleServiceTest extends BaseTest {
 
     @Autowired
     private SampleService sampleService;
+    @Autowired
+    private SampleRepositoryA sampleRepositoryA;
+    @Mock
+    private SampleMapperA sampleMapperA;
 
     @Test
     public void testSampleGet() {
+        // mock database
+        MockUtils.mock(sampleRepositoryA, sampleMapperA);
+        Mockito.when(sampleMapperA.update()).thenReturn(1);
+        // run test
         SampleResp sampleResp = sampleService.sampleGet("test");
         log.info("[测试结果]:{}", JsonUtils.toJson(sampleResp));
     }
